fc9151551173be7e99574da6550dfee2a75a0fc1
Viktor Kapra <viktorkapra@gmail.com>
2024-01-03 20:05:22 +0200

    Adding method for getting quotes

--- server.py
+++ server.py
@@ -3,7 +3,8 @@ import threading
 import time
 import requests
 
-REQUEST = 'https://en.wikipedia.org/w/rest.php/v1/search/page?q='
+REQUEST_WIKI = 'https://en.wikipedia.org/w/rest.php/v1/search/page?q='
+REQUEST_QUOTABLE = 'https://api.quotable.io/quotes?limit=1&author='
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 9999
 ENCODING_DECODING_FORMAT = 'utf-8'
@@ -19,27 +20,50 @@ def new_connection(connection, address):
         # print(f'{message} /') -- for checking the message that was received
         if message == DISCONNECT:
             break
-
-
-        try:
-            request_message_str = REQUEST + message + '&limit=1'
-            print(request_message_str)
-            response = requests.get(request_message_str)
-            result = response.json()
-
-        except requests.exceptions.JSONDecodeError:
-                result = {'title': " ", 'description': "Bad response from API.\n"}
-
-        # print(result) -- for checking the0 response that was receicved
-        if len(result['pages']) == 0 : msg= f" No information about {message} \n"
-        else:
-            msg = f"{result['pages'][0]['title']} \n {result['pages'][0]['description'].rstrip()}.\n"
+        result = get_info_about_author(message)
+        author_name = result[0]
+        msg = result[1]
+        print(author_name)
+        #if author_name != " " :
+        msg += get_quote_from_author(author_name)
 
         connection.send(msg.encode(ENCODING_DECODING_FORMAT))
         time.sleep(2)
         print("Server listens:")
 
     connection.close()
+
+def get_info_about_author(message):
+    try:
+        request_message_str = REQUEST_WIKI + message + '&limit=1'
+        #print(request_message_str)
+        response = requests.get(request_message_str)
+        result = response.json()
+
+    except requests.exceptions.JSONDecodeError:
+        result = {'title': " ", 'description': "Bad response from API.\n"}
+    # print(result)  -- for checking the0 response that was receicved
+    if len(result['pages']) == 0:
+         msg = f" No information about {message} \n"
+    else:
+         msg = f"{result['pages'][0]['title']} \n {result['pages'][0]['description'].rstrip()}.\n"
+    return [result['pages'][0]['title'] , msg]
+
+def get_quote_from_author(author_name):
+    try:
+        request_message_str = REQUEST_QUOTABLE  + author_name
+        print(request_message_str)
+        response = requests.get(request_message_str)
+        result = response.json()
+
+    except requests.exceptions.JSONDecodeError:
+        result = {'title': " ", 'description': "Bad response from API.\n"}
+    print(result) #  -- for checking the0 response that was receicved
+    if result['count'] == 0:
+         quote = f" No quotes  from {author_name} were found \n"
+    else:
+         quote = f"Famous quoute: {result['results'][0]['content']} \n"
+    return quote
 
 
 def start_server():
